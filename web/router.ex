2e00091fd38c1ce3a16b9cfebdf6f9931b39d209
Ahmet Abdi <ahmetabdi@gmail.com>
2016-07-12 09:40:40 +0100

    Add videos

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,7 @@ defmodule Rumbl.Router do
 
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/videos", VideoController
     get "/", PageController, :index
   end
 
