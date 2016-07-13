b1c320f374f2f2338440d2c99923de282df66302
Ahmet Abdi <ahmetabdi@gmail.com>
2016-07-13 10:42:32 +0100

    Add sockets

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,15 +12,10 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-import Player from "./player"
+import socket from './socket'
+import Video from './video'
 
-let video = document.getElementById("video")
-
-if(video) {
-  Player.init(video.id, video.getAttribute("data-player-id"), () => {
-    console.log("Player is ready!")
-  })
-}
+Video.init(socket, document.getElementById("video"))
 
 // Import local files
 //
